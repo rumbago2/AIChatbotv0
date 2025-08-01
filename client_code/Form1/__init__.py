049f1f55ba8b3dbf96fdcc6e8e2caa8ad2239fe5
rumbago2@gmail.com <rumbago2@gmail.com>
2025-08-01 19:03:27 +0000

    Created AI image 1.jpg; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -9,13 +9,13 @@ class Form1(Form1Template):
 
     # Your other code here...
 
-  def categorise_button_click(self, **event_args):
+  def submitllm_click(self, **event_args):
     """This method is called when the button is clicked"""
     # Call the google colab function and pass it the iris measurements
     iris_category = anvil.server.call(
       'predict_iris', 
-      int(self.sepal_length.text),
-      int(self.sepal_width.text),
+      int(self.user_prompt.text),
+      int(self.llm_name.text),
       int(self.petal_length.text),
       int(self.petal_width.text)
     )
@@ -24,3 +24,7 @@ class Form1(Form1Template):
     if iris_category:
       self.species_label.visible = True
       self.species_label.text = "The species is " + iris_category.capitalize()
+
+  def llm_name_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
